5518bb7cabd8042a2d314f64f11b9eaa80560b6d
derkalaender <git@derkalaender.de>
2023-11-19 10:05:28 +0100

    Change images

--- src/frontend/src/pages/DiscoverView.vue
+++ src/frontend/src/pages/DiscoverView.vue
@@ -7,45 +7,33 @@ import RecipeCard from "components/RecipeCard.vue";
 
 const mockData = ref([{
   id: 1,
-  name: "Chicken Stuff 1",
+  name: "Tacos mit Räuchertofu",
   skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
+  img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_1100,q_30,w_2600/hellofresh_s3/image/HF_Y23_R33_W05_DE_R4484-1_KB_Main_low-99964e18.jpg",
   other: undefined
 }, {
   id: 2,
-  name: "Chicken Stuff 2",
+  name: "BBQ Burritos",
   skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
+  img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_1100,q_30,w_2600/hellofresh_s3/image/HF_Y23_R04_W13_DE_L4529-1_Main_low-2df9ba9f.jpg",
   other: undefined
 }, {
   id: 3,
-  name: "Chicken Stuff 3",
+  name: "Elsässer Flammkuchen",
   skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
+  img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_1100,q_30,w_2600/hellofresh_s3/image/HF220706_R30_W35_DE_EXP2953-10_MB_Main_low-ded2657c.jpg",
   other: undefined
 }, {
   id: 4,
-  name: "Chicken Stuff 4",
+  name: "Smokey Hähnchenfilet",
   skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
+  img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_1100,q_30,w_2600/hellofresh_s3/image/R38_W46_DE_R4806-1_Main_low-1832fd6d.jpg",
   other: undefined
 }, {
   id: 5,
-  name: "Chicken Stuff 5",
+  name: "Chorizo-Kartoffelpfanne",
   skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
-  other: undefined
-}, {
-  id: 6,
-  name: "Chicken Stuff 6",
-  skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
-  other: undefined
-}, {
-  id: 7,
-  name: "Chicken Stuff 7",
-  skill: 1,
-  img: "https://hips.hearstapps.com/hmg-prod/images/fennel-roasted-chicken-and-peppers-1639692168.jpg",
+  img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_1100,q_30,w_2600/hellofresh_s3/image/rauchige-argentinos-kartoffelpfanne-mit-peperoni-8f40a537-9a422e72.jpg",
   other: undefined
 }])
 
